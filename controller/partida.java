77172ac89a85bf39f4c0c4264114574ce3d1180e
vitordbo <vitorduarte177@gmail.com>
2022-05-16 08:31:34 -0300

    aumento tela partida e botão para sair

---
+++ controller/partida.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import src.Main;
+
+public class partida implements Initializable {
+    
+    @FXML
+    void sair(ActionEvent event) throws IOException {
+        Main.telaMainMenu();
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        
+    }
+}
