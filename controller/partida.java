b3b19f0fa935929170190580c2f87d26fd89539f
JFelipeBS <f.filipisilva13@hotmail.com>
2022-06-04 23:16:51 -0300

    tela_Partida_Atualizada

--- controller/partida.java
+++ controller/partida.java
@@ -6,10 +6,39 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import view.src.Main;
 
 public class partida implements Initializable {
-    
+
+    @FXML
+    private ImageView imgCard1;
+
+    @FXML
+    private ImageView imgCard2;
+
+    @FXML
+    private ImageView imgCard3;
+
+    @FXML
+    private ImageView imgCard4;
+
+    @FXML
+    private Label pontos_ia;
+
+    @FXML
+    private Label pontos_jogador;
+
+    @FXML
+    private Label NomeJogador;
+
+    @FXML
+    private Label rodada;
+
+    @FXML
+    private Label turno;
+
     @FXML
     void sair(ActionEvent event) throws IOException {
         Main.telaMainMenu();
@@ -18,7 +47,10 @@ public class partida implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
-        
+
+        NomeJogador.setText(Main.getRetorno());
+        pontos_ia.setText("0");
+        pontos_jogador.setText("0");
+
     }
 }
