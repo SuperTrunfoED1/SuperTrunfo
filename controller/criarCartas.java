9be505af2c4470732cdafe3eb31c8e41ce25463d
vitordbo <vitorduarte177@gmail.com>
2022-06-05 16:24:36 -0300

    botão para receber arquivos do computador

--- controller/criarCartas.java
+++ controller/criarCartas.java
@@ -1,6 +1,9 @@
 package controller;
 
+import java.io.File;
 import java.io.IOException;
+
+import javax.swing.JFileChooser;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,4 +16,18 @@ public class criarCartas {
         Main.telaGerador();
     }
 
+    @FXML
+    public void selectFoto (ActionEvent e) {
+    
+        JFileChooser fileChooser = new JFileChooser();
+        
+        fileChooser.setCurrentDirectory(new File("C:\\Users"));
+        //int response = filechooser.showOpenDialog(null); //select file to open
+        int response = fileChooser.showSaveDialog(null); //select file to save
+       
+        if(response == JFileChooser.APPROVE_OPTION) {
+            File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+            System.out.println(file);
+        }
+    }
 }
